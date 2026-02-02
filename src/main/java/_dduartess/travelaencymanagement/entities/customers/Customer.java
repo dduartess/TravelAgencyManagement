c6bcda128a5a19d202d5395526ea631c83f21738
dduartess <dandandsoares@gmail.com>
2026-02-02 10:19:32 -0300

    feat: create @column validations for JPA

--- src/main/java/_dduartess/travelaencymanagement/entities/customers/Customer.java
+++ src/main/java/_dduartess/travelaencymanagement/entities/customers/Customer.java
@@ -2,6 +2,7 @@ package _dduartess.travelaencymanagement.entities.customers;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,7 +19,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "customers")
+@Table(name = "tb_customers")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -31,18 +32,22 @@ public class Customer {
     private Long id;
 
     @NotBlank(message = "O nome é obrigatório")
-    @Pattern(regexp = "^[a-zA-Z\\u00C0-\\u00FF\\s]+$", message = "O nome não pode conter números ou caracteres especiais")
+    @Pattern(regexp = "^[\\p{L}\\s'-]+$", message = "O nome não pode conter números ou caracteres especiais")
+    @Column(name = "name", nullable = false, length = 150)
     private String name;
 
     @NotBlank(message = "O documento é obrigatório")
     @Pattern(regexp = "\\d{7,20}", message = "O documento deve conter apenas números e ter entre 7 e 20 dígitos")
+    @Column(name = "document_number", nullable = false, length = 20)
     private String documentNumber;
 
     @NotNull(message = "A data de nascimento é obrigatória")
     @PastOrPresent(message = "A data de nascimento não pode ser maior que a data atual")
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
     @NotBlank(message = "O telefone é obrigatório")
     @Pattern(regexp = "\\d{11}", message = "O telefone deve conter exatamente 11 dígitos")
+    @Column(name = "phone_number", nullable = false, length = 11)
     private String phoneNumber;
 }
