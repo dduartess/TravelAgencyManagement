82ec9a3d457c002eddba2a0d0d3acb88ce4fea74
dduartess <dandandsoares@gmail.com>
2026-02-02 11:51:35 -0300

    feat: create findDocumentNumber in CustomerRepository

--- src/main/java/_dduartess/travelaencymanagement/repositories/CustomerRepository.java
+++ src/main/java/_dduartess/travelaencymanagement/repositories/CustomerRepository.java
@@ -1,8 +1,12 @@
 package _dduartess.travelaencymanagement.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import _dduartess.travelaencymanagement.entities.customers.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long>{
+
+    Optional<Customer> findByDocumentNumber(String documentNumber);
 
 }
