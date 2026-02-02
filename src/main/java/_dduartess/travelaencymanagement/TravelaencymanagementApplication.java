79a7175bc2c71ee4c0877975bb5422d7706524f4
dduartess <dandandsoares@gmail.com>
2026-02-02 09:49:48 -0300

    feat: initial commit

---
+++ src/main/java/_dduartess/travelaencymanagement/TravelaencymanagementApplication.java
@@ -0,0 +1,13 @@
+package _dduartess.travelaencymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelaencymanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelaencymanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_dduartess/travelaencymanagement/TravelaencymanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package _dduartess.travelaencymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelaencymanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
