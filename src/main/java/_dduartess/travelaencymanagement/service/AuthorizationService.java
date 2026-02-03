f748555d9df0388ff86351b5fdbaf0fdd7d8c693
dduartess <dandandsoares@gmail.com>
2026-02-03 15:49:23 -0300

    feat: create AuthorizationService

---
+++ src/main/java/_dduartess/travelaencymanagement/service/AuthorizationService.java
@@ -0,0 +1,21 @@
+package _dduartess.travelaencymanagement.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import _dduartess.travelaencymanagement.repositories.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByLogin(username);
+    }
+}
